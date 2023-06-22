1ed9c9d1f0aa3736695a7e71918b410a3117fffb
dadilsonmufuma <128637103+dadilsonmufuma@users.noreply.github.com>
2023-06-22 06:01:55 +0100

    Add files via upload

---
+++ contact-us.php
@@ -0,0 +1,362 @@
+<!DOCTYPE html>
+<html lang="pt">
+<!-- cabecalho -->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Mobile Metas responsividade -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Site Metas -->
+    <title>AngoForma - Centro de formção online</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Responsividade CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Customização CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+
+
+
+</head>
+
+<body>
+    <!-- Inicio do Menu Top -->
+    <div class="main-top">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+					<div class="custom-select-box">
+                        <select id="basic" class="selectpicker show-tick form-control" data-placeholder="$ USD">
+							<option>Angola</option>
+					
+						</select>
+                    </div>
+                    <div class="right-phone-box">
+                        <p>Liga para Nós :- <a href="#"> +244 945 396 577</a></p>
+                    </div>
+                    <div class="our-link">
+                        <ul>
+                        
+                            <li><a href="#"><i class="fas fa-location-arrow"></i> Nossa Localização</a></li>
+                            <li><a href="http://localhost:81/AngoForma/contact-us.php"><i class="fas fa-headset"></i> Contacta-nos</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+					
+                    <div class="text-slid-box">
+                        <div id="offer-box" class="carouselTicker">
+                            <ul class="offer-box">
+                                <li>
+                                    <i class="fab fa-opencart"></i> 20% de desconto com o códico: offT80
+                                </li>
+                                <li>
+                                    <i class="fab fa-opencart"></i> 50% - 80% curso informatica
+                                </li>
+                                <li>
+                                    <i class="fab fa-opencart"></i> Off 10%! curso de excel
+                                </li>
+                                <li>
+                                    <i class="fab fa-opencart"></i> Off 50%! Power bi
+                                </li>
+                                <li>
+                                    <i class="fab fa-opencart"></i> Off 10%! telemoveis Iphne
+                                </li>
+                                <li>
+                                    <i class="fab fa-opencart"></i> 50% - 80%  Progamação
+                                </li>
+                                <li>
+                                    <i class="fab fa-opencart"></i> 20% off copia o seu codigo agora: offT30
+                                </li>
+                                <li>
+                                    <i class="fab fa-opencart"></i> Off 50%! cadastra-se agora
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Fim do menu Top -->
+
+    <!-- Incio Memu Top 2 -->
+    <header class="main-header">
+        <!-- Inicio de Navigação -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-default bootsnav">
+            <div class="container">
+                <!-- Inicio Header Navigação -->
+                <div class="navbar-header">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-menu" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa fa-bars"></i>
+                </button>
+                    <a class="navbar-brand" href="index.php"><img src="images/logo.png" class="logo" alt=""></a>
+                </div>
+                <!-- Fim Header Navigação -->
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="navbar-menu">
+                    <ul class="nav navbar-nav ml-auto" data-in="fadeInDown" data-out="fadeOutUp">
+                        <li class="nav-item active"><a class="nav-link" href="index.php">Inicio</a></li>
+                        <li class="nav-item"><a class="nav-link" href="about.php">Sobre Nós</a></li>
+                        <li class="nav-item"><a class="nav-link" href="contact-us.php">Contacta-nos </a></li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+
+                <!--  Atributo inicial de Navegação para pesquisa -->
+                <div class="attr-nav">
+                    <ul>
+                        <li class="search"><a href="#"><i class="fa fa-search"></i></a></li>
+                        <li class="side-menu">
+							<li><a href="http://localhost:81/AngoForma/cadastro/login.php"><i class="fa fa-user s_color"></i> Minha conta</a></li>
+						</li>
+                    </ul>
+                </div>
+                <!-- fim atribuição Navigação  para pesquisa -->
+            </div>
+           
+        </nav>
+        <!-- Fim de Navigação -->
+    </header>
+    <!-- Fim menu Menu Top 2 -->
+    <!--inicio Main Top -->
+
+    <!-- Start Top Search -->
+    <div class="top-search">
+        <div class="container">
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                <input type="text" class="form-control" placeholder="Search">
+                <span class="input-group-addon close-search"><i class="fa fa-times"></i></span>
+            </div>
+        </div>
+    </div>
+    <!-- End Top Search -->
+
+    <!-- inicio da caixa de titulo -->
+    <div class="all-title-box">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2>Contacta-nos </h2>
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.php">Inicio</a></li>
+                        <li class="breadcrumb-item active"> Contacta-nos </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Fim da caixa de titulo -->
+
+    <!-- Start Contact Us  -->
+    <div class="contact-box-main">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-sm-12">
+                    <div class="contact-form-right">
+                        <h2>FAÇA SUA INSCRIÇÃO AGORA MESMO</h2>
+                        <p>Faça já sua inscrição para os variados cursos de diversas areas do saber como engenharia, cienacas, belezas, tecnologias diversas.</p>
+                        <a class="btn hvr-hover" href="http://localhost:81/AngoForma/cadastro/student-create.php">CLICK AQUI E INSCREVA-SE GRÁTIS </a>
+
+                        <div class="contact-info-left">
+                        <h2>INFORMAÇÃO DE COTACTO</h2>
+                        <p>No caso de ter alguma duvida sinta-se a vontade de colocar a sua questão a nossa equipe estará disponivel para ajud-lo. </p>
+                        <ul>
+                            <li>
+                                <p><i class="fas fa-map-marker-alt"></i>Enderenç: Av. 21 janeiro Morro Bento <br>Luanda,<br> KS 87213 </p>
+                            </li>
+                            <li>
+                                <p><i class="fas fa-phone-square"></i>Tel: <a href="tel:+1-888705770">+244 945 396 77</a></p>
+                            </li>
+                            <li>
+                                <p><i class="fas fa-envelope"></i>Email: <a href="mailto:contactinfo@gmail.com">info@angocell.com</a></p>
+                            </li>
+                        </ul>
+                    </div> 
+                        
+                </div>
+				
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- fim -->
+ <!-- inicio Pagina instagram -->
+ <div class="instagram-box">
+        <div class="main-instagram owl-carousel owl-theme">
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="images/categories_img_01.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="images/curso4" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="images/categories_img_02.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="images/curso5.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="images/categories_img_03.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="images/curso6.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="images/categories_img_01.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="images/curso1.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="images/categories_img_02.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="images/curso8.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Fim  Instagram Feed  -->
+
+
+    <!-- Inicio do Footer  -->
+    <footer>
+        <div class="footer-main">
+        <div class="container">
+			
+                <div class="row">
+                    <div class="col-lg-4 col-md-12 col-sm-12">
+                        <div class="footer-widget">
+                            <h4>Sobre a AngoForma</h4>
+                            <p>AngoForma é um centro de formaçõa profissional presencial e online, certificada pelo UNIFOP e com a qualidade exigida pelo ministerio da educação em Angola.</p> 							
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12 col-sm-12">
+                        <div class="footer-link">
+                            <h4>Informação</h4>
+                            <ul>
+                                <li><a href="http://localhost:81/AngoForma/about.php"> Sobre</a></li>
+                                <li><a href="http://localhost:81/AngoForma/contact-us.php">Contacta-nos</a></li>
+                                <li><a href="http://localhost:81/AngoForma/cadastro/register.php">Inscrever</a></li>
+                                <li><a href="http://localhost:81/AngoForma/cadastro/login.php">Entrar</a></li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12 col-sm-12">
+                        <div class="footer-link-contact">
+                            <h4>Contacta-nos</h4>
+                            <ul>
+                                <li>
+                                    <p><i class="fas fa-map-marker-alt"></i>Avenida 21 de Janeiro <br>Morro Bento,<br> casa 67213 </p>
+                                </li>
+                                <li>
+                                    <p><i class="fas fa-phone-square"></i>Tel: <a href="tel:+244">+945 396 577</a></p>
+                                </li>
+                                <li>
+                                    <p><i class="fas fa-envelope"></i>Email: <a href="mailto:contactinfo@gmail.com">info@angocell.com</a></p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    
+    </footer>
+    <!-- Sim Footer  -->
+
+    <!-- Inicio copyright  -->
+    <div class="footer-copyright">
+        <p class="footer-company">Todos os direitos reservados. &copy; 2023 <a href="#">Grupo 2</a> Design por :
+            <a href="https://html.design/">Grupo II</a></p>
+    </div>
+    <!-- fim copyright  -->
+
+    <a href="#" id="back-to-top" title="Back to top" style="display: none;">&uarr;</a>
+
+    <!-- ALL JS FILES -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+    <script src="js/jquery.superslides.min.js"></script>
+    <script src="js/bootstrap-select.js"></script>
+    <script src="js/inewsticker.js"></script>
+    <script src="js/bootsnav.js."></script>
+    <script src="js/images-loded.min.js"></script>
+    <script src="js/isotope.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/baguetteBox.min.js"></script>
+    <script src="js/form-validator.min.js"></script>
+    <script src="js/contact-form-script.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
